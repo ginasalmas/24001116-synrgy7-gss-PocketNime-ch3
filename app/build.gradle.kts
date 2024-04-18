30b7c638c2b11266109d62396f4e6b1f721d2886
Gina Salma sabilla <gina.s.sabilla18@gmail.com>
2024-04-19 04:28:05 +0700

    add resource

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.pocketnime"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pocketnime"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
