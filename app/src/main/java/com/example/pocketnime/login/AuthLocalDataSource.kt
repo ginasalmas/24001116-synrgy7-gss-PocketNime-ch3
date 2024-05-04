a7dc395e39fd7ad74811d5c6feeb5607d61cd211
Gina <144714958+ginasalmas@users.noreply.github.com>
2024-05-05 00:08:08 +0700

    Revert "add login"

--- app/src/main/java/com/example/pocketnime/login/AuthLocalDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pocketnime.login
-
-interface AuthLocalDataSource {
-    fun saveToken(token: String)
-    fun loadToken(): String?
-    fun clearToken()
-
-}
--- app/src/main/java/com/example/pocketnime/login/AuthRemoteDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pocketnime.login
-
-interface AuthRemoteDataSource {
-    suspend fun login(email: String, password:String): String
-}
-
-
